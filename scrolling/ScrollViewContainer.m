4dd78d2fe8e0c1ec6d71ea7e8ff883130762dae8
Pham Dac Loc <pham.loc@gmail.com>
2014-04-22 15:29:11 +0700

    Horizontal scrolling works but not vertical scrolling

---
+++ scrolling/ScrollViewContainer.m
@@ -0,0 +1,31 @@
+//
+//  ScrollViewContainer.m
+//  scrolling
+//
+//  Created by Pham Dac Loc on 4/22/14.
+//  Copyright (c) 2014 Loc D. Pham. All rights reserved.
+//
+
+#import "ScrollViewContainer.h"
+
+@implementation ScrollViewContainer
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
